3a851abd19baec70ab781d650c2a69ce9f086160
Ema <elemadeboca@gmail.com>
2024-06-16 16:48:52 -0300

    se agrego mundial

---
+++ test/ar/edu/unlam/dominio/testGestionMundial.java
@@ -0,0 +1,18 @@
+package ar.edu.unlam.dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class testGestionMundial {
+
+
+	@Test
+	public void queSePuedaCrearUnTorneoCon32Equipos() {
+		
+		
+	
+	}
+
+}
